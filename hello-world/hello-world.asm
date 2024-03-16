22c0a2aa1b47b9d9327c4173cae09d2c2ada00a6
Kai <kai.merklein@web.de>
2024-03-16 20:11:58 +0100

    hello-world

---
+++ hello-world/hello-world.asm
@@ -0,0 +1,22 @@
+  *=$0801
+ 
+  !byte $0c,$08,$e2,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
+
+  lda #$0d            ; Load the CR character into A to start a new line
+  jsr $ffd2           ; Call the CHROUT routine to print it
+
+  ldx #$00            ; Start with the first character of the string
+    
+print_loop
+  lda message,x       ; Load the character at the current index of the message
+  beq done            ; If it's the null terminator (0), we're done
+  jsr $ffd2           ; Call CHROUT with the character in A
+  inx                 ; Move to the next character in the message
+  bne print_loop      ; Loop back if not done
+
+done
+  rts                 ; Return from subroutine, ends the program
+
+message
+ !text "HELLO WORLD!",0 ; The string to print, followed by a null terminator
+
