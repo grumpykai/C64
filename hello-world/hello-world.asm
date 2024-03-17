650c5b39f09fce8aa893ed05217a1172dc715028
Kai <kai.merklein@web.de>
2024-03-17 21:17:51 +0100

    retro assembler

--- hello-world/hello-world.asm
+++ hello-world/hello-world.asm
@@ -1,6 +1,6 @@
   *=$0801
  
-  !byte $0c,$08,$e2,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
+  .byte $0c,$08,$e2,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
 
   lda #$0d            ; Load the CR character into A to start a new line
   jsr $ffd2           ; Call the CHROUT routine to print it
@@ -9,14 +9,17 @@
     
 print_loop
   lda message,x       ; Load the character at the current index of the message
-  beq done            ; If it's the null terminator (0), we're done
+  beq done_it         ; If it's the null terminator (0), we're done
   jsr $ffd2           ; Call CHROUT with the character in A
   inx                 ; Move to the next character in the message
   bne print_loop      ; Loop back if not done
 
-done
+done_it
   rts                 ; Return from subroutine, ends the program
 
 message
- !text "HELLO WORLD!",0 ; The string to print, followed by a null terminator
-
+ .encoding "screencodecommodore", "upper"
+ 
+ .text "HELLO RETRO WORLD IN VICE!" ; The string to print, followed by a null terminator
+ .byte 0
+ .end
---
+++ hello-world/run.bat
@@ -0,0 +1,33 @@
+@echo off
+
+
+REM Check if the local file name parameter is provided
+if "%~1"=="" (
+    echo Usage: %0 local_file_name
+    exit /b 1
+)
+
+REM Set the local file name from the command-line parameter
+set "local_file=%~1"
+
+REM Compile
+..\..\retroassembler\retroassembler.exe -O=PRG %local_file%.asm 
+
+REM Format the Floppy
+..\..\vice\bin\c1541.exe -format "%local_file%,01" d64 %local_file%.d64 
+
+REM copy PRG file to floppy
+..\..\vice\bin\c1541.exe -attach %local_file%.d64 -write %local_file%.prg  %local_file%
+
+if "%~2"=="ftp" (
+    REM Prepare and execute FTP to send the disk to Ultimate-II
+    echo put %local_file%.d64 /Flash/disks/%local_file%.d64 > ftpcmd.dat
+    echo quit >> ftpcmd.dat
+    ftp -A -s:ftpcmd.dat Ultimate-II
+    del ftpcmd.dat
+)
+
+if "%~2"=="vice" (
+    REM Run with VICE
+    ..\..\vice\bin\x64sc.exe -autostart .\%local_file%.d64 && exit 0 || exit 1
+)
